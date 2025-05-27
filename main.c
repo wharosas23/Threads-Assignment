4b8c2d8673c8a0f0b70062286daba1b20a3489b3
wharosas23 <arenasrosasw@spu.edu>
2025-05-27 16:38:59 -0700

    reduced code
    reduced the code using structs to complete the threats

--- main.c
+++ main.c
@@ -12,11 +12,13 @@
 
 void random_number() {
 
+    srand(time(NULL));
+
     // Create a file named "Data.txt"
     FILE *file = fopen("Data.txt", "w");
     
     // In Data.txt, write 10 random numbers between 0 and 100
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 1000000; i++) {
         int num = rand() % 101;
         fprintf(file, "%d\n", num);
     }
@@ -25,11 +27,18 @@ void random_number() {
     fclose(file);    
 }
 
+typedef struct {
+    char label;
+    double *time;
+    const char* filename;
+}thread_data;
+
 void *thread_func_a(void *arg) {
 
-    // Define the variables used to measure the time taken by the thread, and variables to calculate the average of the numbers copied in the Data_Thread.txt file
+    thread_data* t = (thread_data*)arg;
+
     clock_t start, end;
-    double total_time;
+
     long long sum = 0;
     int count = 0;
     int num;
@@ -39,7 +48,7 @@ void *thread_func_a(void *arg) {
 
     // Open the file "Data.txt" for reading and "Data_Thread_A.txt" for writing
     FILE *source_file = fopen("Data.txt", "r");
-    FILE *dest_file = fopen("Data_Thread_A.txt", "w");
+    FILE *dest_file = fopen(t->filename, "w");
 
     // Check if the files were opened successfully
     /***TODO check if there is a better way to close the files*/
@@ -58,119 +67,15 @@ void *thread_func_a(void *arg) {
     
     // If the file is empty, then the average is 0.0, but if it is not empty, then calculate the average
     double average = (count > 0) ? (double)sum / count : 0.0;
-    printf("Thread A Average = %.2f\n", average);
+    printf("Thread %c Average = %.2f\n", t-> label, average);
 
     // End the clock to measure the time
     end = clock();
 
     // Calculate the total time taken by the thread in milliseconds
-    total_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-
-    double *time_ptr = (double *)arg;
-    *time_ptr = total_time;
-
-    // Close the both of the file
-    fclose(source_file);
-    fclose(dest_file);
-
-    return NULL;  // Exit the thread
-}
-
-void *thread_func_b(void *arg) {
-
-    // Define the variables used to measure the time taken by the thread, and variables to calculate the average of the numbers copied in the Data_Thread.txt file
-    clock_t start, end;
-    double total_time;
-    long long sum = 0;
-    int count = 0;
-    int num;
-
-    // Start the clock to measure the time
-    start = clock();
-
-    // Open the file "Data.txt" for reading and "Data_Thread_A.txt" for writing
-    FILE *source_file = fopen("Data.txt", "r");
-    FILE *dest_file = fopen("Data_Thread_B.txt", "w");
-
-    // Check if the files were opened successfully
-    /***TODO check if there is a better way to close the files*/
-    if (source_file == NULL || dest_file == NULL) {
-        perror("Error opening file");
-        pthread_exit(NULL);
-    }
-
-    // If files have been opened successfully, read the numbers from "Data.txt" and copy them to "Data_Thread_A.txt"
-    // While reading, calculate the sum and count of the numbers in the Data.txt file
-    while (fscanf(source_file, "%d", &num) == 1) {
-        fprintf(dest_file, "%d\n", num);
-        sum += num;
-        count++;
-    }
-    
-    // If the file is empty, then the average is 0.0, but if it is not empty, then calculate the average
-    double average = (count > 0) ? (double)sum / count : 0.0;
-    printf("Thread B Average = %.2f\n", average);
-
-    // End the clock to measure the time
-    end = clock();
-
-    // Calculate the total time taken by the thread in milliseconds
-    total_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-
-    double *time_ptr = (double *)arg;
-    *time_ptr = total_time;
-
-
-    // Close the both of the file
-    fclose(source_file);
-    fclose(dest_file);
-
-    return NULL;  // Exit the thread
-}
-
-void *thread_func_c(void *arg) {
-
-    // Define the variables used to measure the time taken by the thread, and variables to calculate the average of the numbers copied in the Data_Thread.txt file
-    clock_t start, end;
-    double total_time;
-    long long sum = 0;
-    int count = 0;
-    int num;
-
-    // Start the clock to measure the time
-    start = clock();
-
-    // Open the file "Data.txt" for reading and "Data_Thread_A.txt" for writing
-    FILE *source_file = fopen("Data.txt", "r");
-    FILE *dest_file = fopen("Data_Thread_C.txt", "w");
-
-    // Check if the files were opened successfully
-    /***TODO check if there is a better way to close the files*/
-    if (source_file == NULL || dest_file == NULL) {
-        perror("Error opening file");
-        pthread_exit(NULL);
-    }
-
-    // If files have been opened successfully, read the numbers from "Data.txt" and copy them to "Data_Thread_A.txt"
-    // While reading, calculate the sum and count of the numbers in the Data.txt file
-    while (fscanf(source_file, "%d", &num) == 1) {
-        fprintf(dest_file, "%d\n", num);
-        sum += num;
-        count++;
-    }
-    
-    // If the file is empty, then the average is 0.0, but if it is not empty, then calculate the average
-    double average = (count > 0) ? (double)sum / count : 0.0;
-    printf("Thread C Average = %.2f\n", average);
-
-    // End the clock to measure the time
-    end = clock();
-
-    // Calculate the total time taken by the thread in milliseconds
-    total_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-
-    double *time_ptr = (double *)arg;
-    *time_ptr = total_time;
+    // total_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+    *(t->time) = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+    //t->time = total_time;  // Store the time taken by the thread in the provided pointer
 
     // Close the both of the file
     fclose(source_file);
@@ -182,31 +87,40 @@ void *thread_func_c(void *arg) {
 int main() {
     clock_t start, end;
     double total_time;
-    double timeA, timeB, timeC;
+
+    pthread_t threads[3];
+    double times[3];
+
+    thread_data args[3] = {
+        {'A', &times[0], "Data_Thread_A.txt"},
+        {'B', &times[1], "Data_Thread_B.txt"},
+        {'C', &times[2], "Data_Thread_C.txt"}
+    };
 
     // Start the clock to measure the time of the main function
     start = clock();
-    srand(time(NULL));  // Seed the random number generator with current time
+
     random_number();
-    pthread_t threadA, threadB, threadC;
 
-    pthread_create(&threadA, NULL, thread_func_a, &timeA);
-    pthread_create(&threadB, NULL, thread_func_b, &timeB);
-    pthread_create(&threadC, NULL, thread_func_c, &timeC);
+    for (int i = 0; i < 3; i++) {
+        pthread_create(&threads[i], NULL, thread_func_a, &args[i]);
+    }
 
-    pthread_join(threadA, NULL);
-    pthread_join(threadB, NULL);
-    pthread_join(threadC, NULL);
+    for (int i = 0; i < 3; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
     // End the clock to measure the time of the main function
     end = clock();
 
     // Calculate the total time taken by the thread in milliseconds
     total_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-    printf("\nThread A time = %.2f ms\n", timeA);
-    printf("Thread B time = %.2f ms\n", timeB);
-    printf("Thread C time = %.2f ms\n\n", timeC);
-    printf("Total time taken by all threads = %.2f ms\n", timeA + timeB + timeC);
+    printf("\nThread A time = %.2f ms\n", times[0]);
+    printf("Thread B time = %.2f ms\n", times[1]);
+    printf("Thread C time = %.2f ms\n\n", times[2]);
+
+    printf("Total time taken by all threads = %.2f ms\n", times[0] + times[1] + times[2]);
     printf("Total time taken by the program = %.2f ms\n", total_time);
-    return 0;  // Exit the program
+
+    return 0;
 }
